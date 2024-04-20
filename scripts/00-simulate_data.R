30d4bcb816d6dfa66dbae757c7df5df0e7df1bb4
Amie Liu <amie.liu@mail.utoronto.ca>
2024-04-20 20:46:25 +0800

    Update table

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -20,8 +20,8 @@ simulated_data <-
   tibble(
     # Use 1 through to 10 to represent each rank
     "Rank" = 1:10,
-    # Randomly pick a number between 0 and 1000
-    "Points Won" = sample(0:1000, 10, replace = TRUE),
+    # Randomly pick a number between 0 and 1000 for MVP points won
+    "MVP Points Won" = sample(0:1000, 10, replace = TRUE),
     # Randomly generate a number between 0 and 1 with three decimal places for field goal percentage 
     "Field Goal Percentage" = round(runif(10, min = 0, max = 1) * 1000) / 1000,
     # Randomly generate a number between 0 and 1 with three decimal places for three points percentage
